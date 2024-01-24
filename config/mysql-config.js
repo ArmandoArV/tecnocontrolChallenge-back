b27be45e79deb55c2c15c79106cfe63718476f55
ArmandoArV <armando.arredondo.valle@gmail.com>
2024-01-23 18:08:17 -0600

    Implements JWT

--- config/mysql-config.js
+++ config/mysql-config.js
@@ -1,21 +1,10 @@
 const mysql = require("mysql2/promise");
 
-const config = {
-  connectionLimit: 10,
-  host: process.env.DBHOST,
-  user: process.env.DBUSER,
-
-  password: process.env.DBPASSWORD,
-  database: "kreidencial",
-  debug: false,
-  port: 3306,
-};
-
 const localConfig = {
   connectionLimit: 10,
   host: "localhost",
   user: "root",
-  database: "kreidencial",
+  database: "tecnocontrol",
   debug: false,
   port: 3306,
 };
